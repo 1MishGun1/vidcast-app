9d3ebf2ac50b425e07018144af23609a913167f8
1MishGun1 <mikhail.kudrov@gmail.com>
2025-05-17 23:00:17 +0300

    import Router.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
+import { Router } from "./router/Router";
 
 function App() {
   return (
-    <div className="App">
-      VidMaker
-    </div>
+    <>
+      <Router />
+    </>
   );
 }
 

7177254aab179456a9c84f6588bbf04269a4f1fa
1MishGun1 <mikhail.kudrov@gmail.com>
2025-05-22 20:51:23 +0300

    create interfaces for video

---
+++ src/models/video.ts
@@ -0,0 +1,24 @@
+export interface IVideo {
+  _id: string;
+  title: string;
+  description: string;
+  tags: string[];
+  views: number;
+  user: {
+    _id: string;
+    login: string;
+    name?: string;
+    surname?: string;
+    avatar?: string;
+  };
+  cover: string;
+  videoUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IVideoState {
+  data: IVideo[];
+  loading: boolean;
+  error: string | null;
+}

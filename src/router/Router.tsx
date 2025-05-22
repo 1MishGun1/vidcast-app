e0ec595e4592904d634efbee219859f6b9aced31
1MishGun1 <mikhail.kudrov@gmail.com>
2025-05-22 20:48:02 +0300

    add layouts

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "../components/Layout/Layout";
+import { HomePage } from "../pages/HomePage";
+import { FullVideoPage } from "../pages/FullVideoPage/FullVideoPage";
+import { AuthLayout } from "../components/AuthLayout/AuthLayout";
+import { LoginPage } from "../pages/LoginPage/LoginPage";
+import { RegisterPage } from "../pages/RegisterPage/RegisterPage";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/videos/:id" element={<FullVideoPage />} />
+      </Route>
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
     </Routes>
   );
 };

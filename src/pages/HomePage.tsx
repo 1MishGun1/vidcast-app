53eff0e575de1e3e5d17622c20d246870d8bdbd9
1MishGun1 <mikhail.kudrov@gmail.com>
2025-05-24 14:39:25 +0300

    debag

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,18 @@
 import { VideoList } from "../components/VideoList/VideoList";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAuthMe, selectIsAuth } from "../features/auth/auth";
+import { useEffect } from "react";
+import { AppDispatch } from "../store";
 
 export const HomePage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const isAuth = useSelector(selectIsAuth);
+  console.log(isAuth);
+
+  useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, [dispatch]);
+
   return (
     <div>
       <VideoList />

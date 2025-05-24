2c0d91689bb0c1bbd1f7c8bd5a5b2e8824c212a4
1MishGun1 <mikhail.kudrov@gmail.com>
2025-05-24 12:41:39 +0300

    add validate form

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,39 +1,78 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import Styles from "./LoginPage.module.css";
+import { ILoginUser } from "../../models/user";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserData, selectIsAuth } from "../../features/auth/auth";
+import { AppDispatch } from "../../store";
 
 export const LoginPage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const isAuth = useSelector(selectIsAuth);
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm<ILoginUser>({
+    defaultValues: {
+      login: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit: SubmitHandler<ILoginUser> = async (values) => {
+    const data = await dispatch(fetchUserData(values));
+    if (fetchUserData.fulfilled.match(data)) {
+      const token = data.payload.tokenUser;
+      window.localStorage.setItem("token", token);
+    } else {
+      console.error("Ошибка авторизации");
+    }
+  };
+
+  if (isAuth) return <Navigate to={"/"} />;
+
+  console.log(isAuth);
+
   return (
     <section className={Styles["login_page"]}>
       <img src="/img/1.jpg" alt="" className={Styles["login_img"]} />
       <div className={Styles["login_form_section"]}>
-          <form className={Styles["form_login"]}>
-            <h1 className={Styles["login_title"]}>Войти</h1>
-            <div className={Styles["form_row"]}>
-              <label htmlFor="login">Логин</label>
-              <input
-                type="text"
-                name="login"
-                id="login"
-                className={Styles["form_input"]}
-              />
-            </div>
-            <div className={Styles["form_row"]}>
-              <label htmlFor="password">Пароль</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                className={Styles["form_input"]}
-              />
-            </div>
-            <button className={Styles["form_btn"]}>Войти</button>
-            <div className={Styles["form_register"]}>
-              <p className={Styles["form_question"]}>Нет аккаунта?</p>
-              <Link to={"/register"} className={Styles["form_link_register"]}>
-                Создайте его!
-              </Link>
-            </div>
-          </form>
+        <form
+          className={Styles["form_login"]}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <h1 className={Styles["login_title"]}>Войти</h1>
+          <div className={Styles["form_row"]}>
+            <label htmlFor="login">Логин</label>
+            <input
+              type="text"
+              id="login"
+              className={Styles["form_input"]}
+              {...register("login", { required: "Укажите логин" })}
+            />
+          </div>
+          <div className={Styles["form_row"]}>
+            <label htmlFor="password">Пароль</label>
+            <input
+              type="password"
+              id="password"
+              className={Styles["form_input"]}
+              {...register("password", { required: "Укажите пароль" })}
+            />
+          </div>
+          <button type="submit" className={Styles["form_btn"]}>
+            Войти
+          </button>
+          <div className={Styles["form_register"]}>
+            <p className={Styles["form_question"]}>Нет аккаунта?</p>
+            <Link to={"/register"} className={Styles["form_link_register"]}>
+              Создайте его!
+            </Link>
+          </div>
+        </form>
       </div>
     </section>
   );

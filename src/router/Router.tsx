4796d4b3a477272b1a8448415dd38964bb53c445
Mikhail Kudrov <110538891+1MishGun1@users.noreply.github.com>
2025-05-26 19:28:48 +0300

    add routes for create video and chanel

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,6 +5,8 @@ import { FullVideoPage } from "../pages/FullVideoPage/FullVideoPage";
 import { AuthLayout } from "../components/AuthLayout/AuthLayout";
 import { LoginPage } from "../pages/LoginPage/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage/RegisterPage";
+import { CreateVideoPage } from "../pages/CreateVideoPage/CreateVideoPage";
+import { ChanelPage } from "../pages/ChanelPage/ChanelPage";
 
 export const Router = () => {
   return (
@@ -12,6 +14,8 @@ export const Router = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="/videos/:id" element={<FullVideoPage />} />
+        <Route path="/create/video" element={<CreateVideoPage />} />
+        <Route path="/chanel/:id" element={<ChanelPage />} />
       </Route>
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<LoginPage />} />

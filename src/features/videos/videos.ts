942d68263493030816681da95794b1ba311fc65c
1MishGun1 <mikhail.kudrov@gmail.com>
2025-05-27 21:44:27 +0300

    add logic for tags and add video

--- src/features/videos/videos.ts
+++ src/features/videos/videos.ts
@@ -1,16 +1,41 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import axios from "../../api/config";
-import { IVideo, IVideoState } from "../../models/video";
+import { IVideo, IVideoCreate, IVideoState } from "../../models/video";
 
 export const getAllVideos = createAsyncThunk("videos/getVideos", async () => {
   const { data } = await axios.get("/videos");
   return data;
 });
 
+export const getVideoTags = createAsyncThunk(
+  "videos/getVideoTags",
+  async () => {
+    const { data } = await axios.get("/tags");
+    return data;
+  }
+);
+
+export const createVideo = createAsyncThunk<IVideo, IVideoCreate>(
+  "videos/createVideo",
+  async (params) => {
+    try {
+      const { data } = await axios.post("/videos", params);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
+
 const initialState: IVideoState = {
   data: [],
   loading: false,
   error: null,
+  tags: {
+    data: [],
+    loading: false,
+    error: null,
+  },
 };
 
 export const videoSlice = createSlice({
@@ -19,6 +44,7 @@ export const videoSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
+      // Get all videos
       .addCase(getAllVideos.pending, (state) => {
         state.loading = true;
         state.error = null;
@@ -31,8 +57,42 @@ export const videoSlice = createSlice({
         }
       )
       .addCase(getAllVideos.rejected, (state) => {
-        state.loading = false;
-        state.error = "Error loading videos";
+        state.tags.loading = false;
+        state.tags.error = "Error loading videos";
+      })
+
+      // Create video
+      .addCase(createVideo.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        createVideo.fulfilled,
+        (state, action: PayloadAction<IVideo>) => {
+          state.loading = true;
+          state.data.unshift(action.payload);
+        }
+      )
+      .addCase(createVideo.rejected, (state) => {
+        state.loading = true;
+        state.error = "Error create video";
+      })
+
+      // Get tags
+      .addCase(getVideoTags.pending, (state) => {
+        state.tags.loading = true;
+        state.tags.error = null;
+      })
+      .addCase(
+        getVideoTags.fulfilled,
+        (state, action: PayloadAction<string[]>) => {
+          state.tags.loading = false;
+          state.tags.data = action.payload;
+        }
+      )
+      .addCase(getVideoTags.rejected, (state) => {
+        state.tags.loading = false;
+        state.tags.error = "Error loading videos";
       });
   },
 });

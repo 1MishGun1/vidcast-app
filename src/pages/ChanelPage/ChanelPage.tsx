800b7c865d10780736ba5d7eb89648c67da604b6
1MishGun1 <mikhail.kudrov@gmail.com>
2025-05-30 18:34:38 +0300

    create ChanelPage

--- src/pages/ChanelPage/ChanelPage.tsx
+++ src/pages/ChanelPage/ChanelPage.tsx
@@ -1,5 +1,45 @@
+import { useDispatch, useSelector } from "react-redux";
 import Styles from "./ChanelPage.module.css";
+import { useEffect } from "react";
+import { getUserById } from "../../features/auth/auth";
+import { useParams } from "react-router-dom";
+import { RootState, AppDispatch } from "../../store";
+import { HeaderChanel } from "../../components/HeaderChanel/HeaderChanel";
+import { InfoUserChanel } from "../../components/InfoUserChanel/InfoUserChanel";
 
 export const ChanelPage = () => {
-  return <div>ChanelPage</div>;
+  const { id } = useParams();
+  const dispatch = useDispatch<AppDispatch>();
+  const {
+    selectedUser: user,
+    loading,
+    error,
+  } = useSelector((state: RootState) => state.auth);
+  const theme = useSelector((state: RootState) => state.theme.currentTheme);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(getUserById(id));
+    }
+  }, [dispatch, id]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!user) return <div>User not found</div>;
+
+  return (
+    <section className={Styles["chanel_page"]}>
+      <InfoUserChanel
+        avatar={user.avatar}
+        name={user.name}
+        surname={user.surname}
+        login={user.login}
+        theme={theme}
+      />
+      <HeaderChanel userId={user._id} theme={theme} />
+      <div className={Styles["chanel_last_videos"]}>
+        <h2 className={Styles["chanel_last_videos_title"]}>Последние видео</h2>
+      </div>
+    </section>
+  );
 };

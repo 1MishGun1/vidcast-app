5d2575995fd8c071b647076d4c5d4b70bb395900
1MishGun1 <mikhail.kudrov@gmail.com>
2025-05-30 18:31:40 +0300

    delete strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <ThemeProvider>
         <BrowserRouter>
@@ -19,7 +19,7 @@ root.render(
         </BrowserRouter>
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function

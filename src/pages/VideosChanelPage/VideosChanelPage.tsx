f8fed8c05dd039b98443d54182a345c9826b8df9
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-01 12:04:45 +0300

    add userVideos

--- src/pages/VideosChanelPage/VideosChanelPage.tsx
+++ src/pages/VideosChanelPage/VideosChanelPage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getUserById } from "../../features/auth/auth";
@@ -6,6 +6,8 @@ import { RootState, AppDispatch } from "../../store";
 import Styles from "./VideosChanelPage.module.css";
 import { HeaderChanel } from "../../components/HeaderChanel/HeaderChanel";
 import { InfoUserChanel } from "../../components/InfoUserChanel/InfoUserChanel";
+import { getUserVideo } from "../../features/videos/videos";
+import { VideoCard } from "../../components/VideoCard/VideoCard";
 
 export const VideosChanelPage = () => {
   const { id } = useParams();
@@ -16,10 +18,14 @@ export const VideosChanelPage = () => {
     error,
   } = useSelector((state: RootState) => state.auth);
   const theme = useSelector((state: RootState) => state.theme.currentTheme);
+  const { data: userVideos, loading: videosLoading } = useSelector(
+    (state: RootState) => state.video.userVideos
+  );
 
   useEffect(() => {
     if (id) {
       dispatch(getUserById(id));
+      dispatch(getUserVideo(id));
     }
   }, [dispatch, id]);
 
@@ -39,6 +45,13 @@ export const VideosChanelPage = () => {
       <HeaderChanel userId={user._id} theme={theme} />
       <div className={Styles["chanel_last_videos"]}>
         <h2 className={Styles["chanel_last_videos_title"]}>Видео</h2>
+        {videosLoading ? (
+          <p>Загрузка видео...</p>
+        ) : userVideos.length > 0 ? (
+          userVideos.map((video) => <VideoCard key={video._id} {...video} />)
+        ) : (
+          <p>Пользователь еще не загрузил видео</p>
+        )}
       </div>
     </section>
   );

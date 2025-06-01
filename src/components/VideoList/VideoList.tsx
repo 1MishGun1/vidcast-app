9b70e7650f6a0aa157bf7a601e31564757e3b8b5
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-01 12:02:49 +0300

    import VideoCard

--- src/components/VideoList/VideoList.tsx
+++ src/components/VideoList/VideoList.tsx
@@ -3,8 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getAllVideos, getVideoTags } from "../../features/videos/videos";
 import { RootState, AppDispatch } from "../../store";
 import Styles from "./VideoList.module.css";
-import { Link } from "react-router-dom";
-// import { getUserById } from "../../features/auth/auth";
+import { VideoCard } from "../VideoCard/VideoCard";
 
 export const VideoList = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -26,42 +25,7 @@ export const VideoList = () => {
     <>
       <div className={Styles["video__items"]}>
         {data.map((video) => (
-          <Link
-            to={`/videos/${video._id}`}
-            key={video._id}
-            className={Styles["video__item"]}
-          >
-            <div className={Styles["video_item_preview"]}></div>
-            {/* <img src={video.videoUrl} alt={video.title} width="200" /> */}
-            <div className={Styles["video_item_info"]}>
-              {/* <div className={Styles["video_item_avatar"]}></div> */}
-              <div className={Styles["video_item_text"]}>
-                <h4 className={Styles["video_item_title"]}>{video.title}</h4>
-                {/* <p>
-                  {tags.map((tag) => (
-                    <span key={tag}>{tag}</span>
-                  ))}
-                </p> */}
-                <Link
-                  to={`/chanel/${video.user._id}`}
-                  className={Styles["video_item_login"]}
-                >
-                  {video.user.login}
-                </Link>
-                <div className={Styles["video_item_details"]}>
-                  <p className={Styles["video_item_views"]}>
-                    {video.views} просмотров
-                  </p>
-                  <p>•</p>
-                  <p className={Styles["video_item_date"]}>
-                    {new Date(video.createdAt).getDate()}.
-                    {new Date(video.createdAt).getMonth()}.
-                    {new Date(video.createdAt).getFullYear()}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Link>
+          <VideoCard key={video._id} {...video} />
         ))}
       </div>
     </>

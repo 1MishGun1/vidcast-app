e61511895007d07df7446e81da2e66996c74da46
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-01 12:03:16 +0300

    show video user

--- src/pages/ChanelPage/ChanelPage.tsx
+++ src/pages/ChanelPage/ChanelPage.tsx
@@ -2,10 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import Styles from "./ChanelPage.module.css";
 import { useEffect } from "react";
 import { getUserById } from "../../features/auth/auth";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { RootState, AppDispatch } from "../../store";
 import { HeaderChanel } from "../../components/HeaderChanel/HeaderChanel";
 import { InfoUserChanel } from "../../components/InfoUserChanel/InfoUserChanel";
+import { getUserVideo } from "../../features/videos/videos";
 
 export const ChanelPage = () => {
   const { id } = useParams();
@@ -17,9 +18,14 @@ export const ChanelPage = () => {
   } = useSelector((state: RootState) => state.auth);
   const theme = useSelector((state: RootState) => state.theme.currentTheme);
 
+  const { data: userVideos, loading: videosLoading } = useSelector(
+    (state: RootState) => state.video.userVideos
+  );
+
   useEffect(() => {
     if (id) {
       dispatch(getUserById(id));
+      dispatch(getUserVideo(id));
     }
   }, [dispatch, id]);
 
@@ -39,6 +45,47 @@ export const ChanelPage = () => {
       <HeaderChanel userId={user._id} theme={theme} />
       <div className={Styles["chanel_last_videos"]}>
         <h2 className={Styles["chanel_last_videos_title"]}>Последние видео</h2>
+        <div className={Styles["chanel_last_videos_items"]}>
+          {videosLoading ? (
+            <p>Загрузка видео...</p>
+          ) : userVideos.length > 0 ? (
+            userVideos.map((video) => (
+              <Link
+                to={`/videos/${video._id}`}
+                key={video._id}
+                className={Styles["video__item"]}
+              >
+                <div className={Styles["video_item_preview"]}></div>
+                <div className={Styles["video_item_info"]}>
+                  <div className={Styles["video_item_text"]}>
+                    <h4 className={Styles["video_item_title"]}>
+                      {video.title}
+                    </h4>
+                    <Link
+                      to={`/chanel/${video.user._id}`}
+                      className={Styles["video_item_login"]}
+                    >
+                      {video.user.login}
+                    </Link>
+                    <div className={Styles["video_item_details"]}>
+                      <p className={Styles["video_item_views"]}>
+                        {video.views} просмотров
+                      </p>
+                      <p>•</p>
+                      <p className={Styles["video_item_date"]}>
+                        {new Date(video.createdAt).getDate()}.
+                        {new Date(video.createdAt).getMonth()}.
+                        {new Date(video.createdAt).getFullYear()}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <p>Пользователь еще не загрузил видео</p>
+          )}
+        </div>
       </div>
     </section>
   );

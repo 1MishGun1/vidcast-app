249e484b4687ad086aa0416b3faca0a0f6059958
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-03 07:14:43 +0300

    create TrendsPage

---
+++ src/pages/TrendsPage/TrendsPage.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import Styles from "./TrendsPage.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import { getTrendingVideos } from "../../features/videos/videos";
+import { VideoCard } from "../../components/VideoCard/VideoCard";
+
+export const TrendsPage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { data, loading, error } = useSelector(
+    (state: RootState) => state.video
+  );
+
+  useEffect(() => {
+    dispatch(getTrendingVideos());
+  }, [dispatch]);
+
+  if (loading) return <p>Загрузка трендов...</p>;
+  if (error) return <p>Ошибка: {error}</p>;
+  if (!data.length) return <p>Трендовых видео пока нет.</p>;
+
+  return (
+    <section className={Styles["trend_page"]}>
+      <h2 className={Styles["trend_title"]}>Тренды</h2>
+      <div className={Styles["trend_items"]}>
+        {data.map((video) => (
+          <VideoCard
+            key={video._id}
+            _id={video._id}
+            cover={video.cover}
+            videoUrl={video.videoUrl}
+            title={video.title}
+            user={video.user}
+            views={video.views}
+            createdAt={video.createdAt}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};

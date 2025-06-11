c899149762324937284f182cd732522247e2da08
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-11 17:58:34 +0300

    del useCallback

--- src/components/VideoDropzone/VideoDropzone.tsx
+++ src/components/VideoDropzone/VideoDropzone.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState } from "react";
 import Styles from "./VideoDropzone.module.css";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";

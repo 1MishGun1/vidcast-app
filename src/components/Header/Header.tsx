401d1b373338f6c50ac2f056c8f2cf95fbc1d16c
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-11 18:01:56 +0300

    import Search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,18 @@
 import Styles from "./Header.module.css";
-import { IoMdSearch } from "react-icons/io";
 import { BsCameraVideo } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleTheme } from "../../features/theme/theme";
+import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { selectIsAuth } from "../../features/auth/auth";
+import { Search } from "../Search/Search";
 
 export const Header = () => {
-  const dispatch = useDispatch();
   const theme = useSelector((state: RootState) => state.theme.currentTheme);
   const isAuth = useSelector(selectIsAuth);
 
   return (
     <header className={Styles["header"]}>
-      <div className={Styles["search__block"]}>
-        <input
-          type="text"
-          name="search"
-          placeholder="Введите запрос"
-          className={Styles["search__input"]}
-          data-theme={theme}
-        />
-        <button className={Styles["search__btn"]} data-theme={theme}>
-          <IoMdSearch size={24} />
-        </button>
-        <button onClick={() => dispatch(toggleTheme())}>
-          {theme === "light" ? "🌙" : "☀️"} 
-        </button>
-      </div>
+      <Search />
       {isAuth ? (
         <Link
           to={"/create/video"}

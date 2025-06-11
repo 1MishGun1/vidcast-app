e90fa404a9129558e790e8bb78c6663009a3290d
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-11 17:57:55 +0300

    add redirect on home

--- src/pages/TrendsPage/TrendsPage.tsx
+++ src/pages/TrendsPage/TrendsPage.tsx
@@ -4,12 +4,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import { getTrendingVideos } from "../../features/videos/videos";
 import { VideoCard } from "../../components/VideoCard/VideoCard";
+import { Link } from "react-router-dom";
 
 export const TrendsPage = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { data, loading, error } = useSelector(
     (state: RootState) => state.video
   );
+  const theme = useSelector((state: RootState) => state.theme.currentTheme);
 
   useEffect(() => {
     dispatch(getTrendingVideos());
@@ -17,7 +19,15 @@ export const TrendsPage = () => {
 
   if (loading) return <p>Загрузка трендов...</p>;
   if (error) return <p>Ошибка: {error}</p>;
-  if (!data.length) return <p>Трендовых видео пока нет.</p>;
+  if (!data.length)
+    return (
+      <div className={Styles["no_trends"]}>
+        <p className={Styles["trend_no_trends"]}>Трендовых видео пока нет</p>
+        <Link to={"/"} className={Styles["no_trends_home"]} data-theme={theme}>
+          Вернуться на главную
+        </Link>
+      </div>
+    );
 
   return (
     <section className={Styles["trend_page"]}>
@@ -28,7 +38,7 @@ export const TrendsPage = () => {
             key={video._id}
             _id={video._id}
             cover={video.cover}
-            videoUrl={video.videoUrl}
+            hlsUrl={video.hlsUrl}
             title={video.title}
             user={video.user}
             views={video.views}

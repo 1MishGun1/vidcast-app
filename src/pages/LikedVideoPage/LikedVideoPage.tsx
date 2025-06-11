c1dd1c74122311901a7f2ba9d4b58a416b15b027
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-11 18:02:27 +0300

    add redirect on home

--- src/pages/LikedVideoPage/LikedVideoPage.tsx
+++ src/pages/LikedVideoPage/LikedVideoPage.tsx
@@ -4,12 +4,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../../store";
 import { getLikedVideo } from "../../features/videos/videos";
 import { VideoCard } from "../../components/VideoCard/VideoCard";
+import { Link } from "react-router-dom";
 
 export const LikedVideoPage = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { data, loading, error } = useSelector(
     (state: RootState) => state.video
   );
+  const theme = useSelector((state: RootState) => state.theme.currentTheme);
 
   useEffect(() => {
     dispatch(getLikedVideo());
@@ -17,6 +19,15 @@ export const LikedVideoPage = () => {
 
   if (loading) return <p>Загрузка...</p>;
   if (error) return <p>Ошибка: {error}</p>;
+  if (!data.length)
+    return (
+      <div className={Styles["no_likes"]}>
+        <p className={Styles["like_no_likes"]}>Понравившихся видео пока нет</p>
+        <Link to={"/"} className={Styles["no_likes_home"]} data-theme={theme}>
+          Вернуться на главную
+        </Link>
+      </div>
+    );
 
   return (
     <section className={Styles["like_page"]}>

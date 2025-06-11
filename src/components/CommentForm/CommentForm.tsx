c22eb5bc1766e3d68392b8e0c9edb6d671958c1e
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-11 17:57:06 +0300

    edit CommentForm

--- src/components/CommentForm/CommentForm.tsx
+++ src/components/CommentForm/CommentForm.tsx
@@ -4,21 +4,40 @@ import { createComment } from "../../features/comments/comments";
 import { selectCurrentUser } from "../../features/auth/auth";
 import { AppDispatch, RootState } from "../../store";
 import Styles from "./CommentForm.module.css";
+import { IComment } from "../../models/comment";
 
 interface ICommentForm {
   videoId: string;
+  parentComment?: IComment | null;
+  onCancelReply?: () => void;
 }
 
-export const CommentForm = ({ videoId }: ICommentForm) => {
+export const CommentForm = ({
+  videoId,
+  parentComment = null,
+  onCancelReply,
+}: ICommentForm) => {
   const [text, setText] = useState("");
   const dispatch = useDispatch<AppDispatch>();
   const user = useSelector(selectCurrentUser);
+  const theme = useSelector((state: RootState) => state.theme.currentTheme);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (text.trim()) {
-      dispatch(createComment({ videoId, text }));
+      dispatch(
+        createComment({
+          text,
+          videoId,
+          parentCommentId: parentComment?._id ?? null,
+        })
+      );
       setText("");
+
+      // Закрыть форму ответа (если это ответ)
+      if (parentComment && onCancelReply) {
+        onCancelReply();
+      }
     }
   };
 
@@ -27,12 +46,30 @@ export const CommentForm = ({ videoId }: ICommentForm) => {
       <input
         value={text}
         onChange={(e) => setText(e.target.value)}
-        placeholder="Введите комментарий"
+        placeholder={
+          parentComment ? "Введите ответ..." : "Введите комментарий..."
+        }
         className={Styles["comment_input"]}
+        data-theme={theme}
       />
-      <button type="submit" className={Styles["comment_btn"]}>
-        Комментировать
+      <button
+        type="submit"
+        className={Styles["comment_btn"]}
+        data-theme={theme}
+      >
+        {parentComment ? "Ответить" : "Комментировать"}
       </button>
+
+      {parentComment && onCancelReply && (
+        <button
+          type="button"
+          onClick={onCancelReply}
+          className={Styles["comment_btn"]}
+          data-theme={theme}
+        >
+          Отмена
+        </button>
+      )}
     </form>
   );
 };

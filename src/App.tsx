c6eaaccf026e819b5c0f941be2f09e2ed195cf5d
1MishGun1 <mikhail.kudrov@gmail.com>
2025-06-16 21:55:48 +0300

    add fetchAuthMe

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../src/store";
+import { fetchAuthMe } from "./features/auth/auth";
 import { Router } from "./router/Router";
+import { useEffect } from "react";
 
 const App = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, [dispatch]);
+
   return (
     <>
       <Router />
